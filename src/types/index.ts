ad9e55dc47c78a9f99bd996498fc9d354fa1cc68
Arseniy Danov <Danov2005@yandex.ru>
2025-02-25 23:34:31 +0300

    initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,49 @@
+import { IEvents } from "../components/base/events";
+
+export interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+export interface IOrderForm {
+    payment?: string;
+    address?: string;
+    phone?: string;
+    email?: string;
+    total?: string | number;
+}
+
+export interface IOrder extends IOrderForm {
+    items: string[];
+}
+
+export type OrderError = Partial<Record<keyof IOrder, string>>;
+
+export interface IOrderResponse {
+    id: string;
+    total: number;
+}
+
+export interface IBasket {
+    items: Map<string, number>;
+    add(id: string): void;
+    remove(id: string): void;
+}
+
+export interface ICatalog {
+    items: IProduct;
+    setItems(items: IProduct[]): void;
+    getProduct(id: string): IProduct;
+}
+
+export interface IView {
+    render(data?: object): HTMLElement;
+}
+
+export interface IViewConstructor {
+    new(container: HTMLElement, events?: IEvents): IView;
+}
